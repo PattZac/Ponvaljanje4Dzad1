b4e1308d8b4e7df4f215c07ae9ce41a6fb5abc2d
Pat <113007904+PattZac@users.noreply.github.com>
2023-09-11 10:30:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponvaljanje4Dzad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100, b = 3;
+            Console.WriteLine(a+" dijeleno s "+b+" je "+(a/b)+" i ostatak je "+(a%b));
+            Console.ReadLine();
+        }
+    }
+}
